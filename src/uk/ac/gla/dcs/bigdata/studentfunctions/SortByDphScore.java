1aaff2bc5801e5ef924e6a04946893c586986573
Kanha Bansal <2789927b@student.gla.ac.uk>
2023-02-24 02:29:29 +0000

    minor bug fix

--- src/uk/ac/gla/dcs/bigdata/studentfunctions/SortByDphScore.java
+++ src/uk/ac/gla/dcs/bigdata/studentfunctions/SortByDphScore.java
@@ -5,26 +5,46 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
-import org.apache.spark.api.java.function.FlatMapGroupsFunction;
+import org.apache.spark.api.java.function.MapGroupsFunction;
+import org.apache.spark.broadcast.Broadcast;
 
 import scala.Tuple3;
+import uk.ac.gla.dcs.bigdata.providedstructures.DocumentRanking;
 import uk.ac.gla.dcs.bigdata.providedstructures.NewsArticle;
+import uk.ac.gla.dcs.bigdata.providedstructures.Query;
+import uk.ac.gla.dcs.bigdata.providedstructures.RankedResult;
 
-public class SortByDphScore implements FlatMapGroupsFunction<String, Tuple3<String, NewsArticle, Double>, Tuple3<String, NewsArticle, Double>> {
+public class SortByDphScore implements MapGroupsFunction<String, Tuple3<String, NewsArticle, Double>, DocumentRanking> {
 	/**
 	 *
 	 */
 	private static final long serialVersionUID = -7963085385885927574L;
 
+	List<Query> allQueries;
+	public SortByDphScore() {}
+	public SortByDphScore(Broadcast<List<Query>> allQueries) {
+		this.allQueries = allQueries.getValue();
+	}
 	@Override
-	public Iterator<Tuple3<String, NewsArticle, Double>> call(String key, Iterator<Tuple3<String, NewsArticle, Double>> tuples) {
-		List<Tuple3<String, NewsArticle, Double>> rankedResult = new ArrayList<>();
+	public DocumentRanking call(String key, Iterator<Tuple3<String, NewsArticle, Double>> tuples) {
+		List<RankedResult> rankedResult = new ArrayList<>();
 		List<Tuple3<String, NewsArticle, Double>> sortedTuples = new ArrayList<>();
 		while (tuples.hasNext()) {
 			sortedTuples.add(tuples.next());
 		}
 		Collections.sort(sortedTuples, Collections.reverseOrder((t1, t2) -> Double.compare(t1._3(), t2._3())));
 
-		return sortedTuples.iterator();
+		return (new DocumentRanking(getMatchingQuery(this.allQueries,key), rankedResult));
+	}
+
+	public Query getMatchingQuery (List<Query> allquery, String key) {
+		Query result = null;
+		for(Query qItem : allquery) {
+			if(qItem.getOriginalQuery().contains(key)) {
+				result = qItem;
+				break;
+			}
+		}
+		return result;
 	}
 }
